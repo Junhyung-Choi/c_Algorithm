319306a31bbaac4f19b446381bbc47077d361361
Junhyung Choi <junhyung9571@naver.com>
2020-06-27 12:51:16 +0900

    add Linear_Search.c

---
+++ Algorithm/Linear_Search.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int ls(int* arr,int inp){
+
+}
+int main(void){
+    int arr[10] = {0,1,2,3,4,5,6,7,8,9};
+    int inp,result;
+    printf("%s\n","Put numbers which you wanna find");
+    scanf("%d",&inp);
+    result = ls(arr,inp);
+    print("%d %s %d\n",inp," is at ",result);
+    return 0;
+}
