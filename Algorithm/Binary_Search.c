1aa66d5c75172602c54f5037722fa9af9c727ea9
Junhyung-Choi <junhyung9571@naver.com>
2020-06-28 13:37:32 +0900

    fix Binary_Search.c

--- Algorithm/Binary_Search.c
+++ Algorithm/Binary_Search.c
@@ -4,26 +4,29 @@ int main(void){
     int len = 10;
     int start = 0;
     int end = len-1;
-    int mid;
-    int target;
+    int mid = 0;
+    int target = 0;
     int index = 999;
+    int cnt = 0;
     scanf("%d",&target);
-    while(end - start >=0){
+    while(end - start >= 0 ){
         mid = (start + end) / 2;
         if (arr[mid] == target){
-            index = mid;
+            index = mid+1;
+            break;
         }
         else if(arr[mid] < target){
-            end = mid-1;
-        }
-        else if(arr[mid] > target){
             start = mid+1;
         }
+        else if(arr[mid] > target){
+            end = mid-1;
+        }
+        cnt +=1;
     }
     if(index == 999){
         printf("Couldn't find the number\n");
     }else{
-        printf("The number is at %d",index);
+        printf("The number is at %d\n",index);
     }
 
 }
