f0ac02fcacdad28789d4a447aab657318a1f7156
Junhyung-Choi <junhyung9571@naver.com>
2020-06-30 20:24:21 +0900

    Fix Quick_Sort.c

--- Algorithm/Sort/Quick_Sort.c
+++ Algorithm/Sort/Quick_Sort.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 
 int arr[10] = {1,4,5,8,7,2,3,6,0,9};
-int quick_sort(int* arr,int* left, int* right);
-int main(void){
+int quick_sort(int* arr,int left, int right){
+    if (left>=right){
+        return 0;
+    }
+    int pivot = left;
+    int i = pivot + 1;
+    int j = right;
+    int tmp;
+    while(i<=j){
+        while(i<=right && arr[i] <= arr[pivot]){
+            ++i;
+        }
+        while(j>left && arr[j] >= arr[pivot]){
+            --j;
+        }
+        if (i>j){
+            tmp = arr[j];
+            arr[j] = arr[pivot];
+            arr[pivot] = tmp;
+        }else{
+            tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+    quick_sort(arr,left,j-1);
+    quick_sort(arr,j+1,right);
+}
 
+int main(void){ 
+    quick_sort(arr, 0, 9); // 결과 확인 
+    for(int i=0; i<10; i++){ 
+        printf("%d ", arr[i]); 
+    } 
+    printf("\n");
+    return 0; 
 }
