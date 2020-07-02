102cf41216bdfe5f9f6b42d8330de98d8f464c61
Junhyung-Choi <junhyung9571@naver.com>
2020-07-02 20:19:12 +0900

    Add Inserstion_Sort.c

---
+++ Algorithm/Sort/Insertion_Sort.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void insertion_sort(int* arr, int len);
+int main(void){
+    int arr[9] = {5,3,4,7,2,8,6,9,1};
+    insertion_sort(arr,9);
+    for(int i=0; i<9; ++i){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+void insertion_sort(int* arr, int len){
+    int tmp;
+    for (int i=0;i<len-1;++i){
+        for (int j= i+1;j<len;++j){
+            if(arr[i] > arr[j]){
+                tmp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+    }
+}
