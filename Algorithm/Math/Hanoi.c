f1de97290e9da29bad2f23de9e93f506d8d2d9c6
Junhyung Choi <junhyung9571@naver.com>
2020-07-12 20:14:50 +0900

    Create Hanoi.c

---
+++ Algorithm/Math/Hanoi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+void hanoi_tower(int n,char from,char temp,char to)
+{
+   if(n==1)
+      printf("원판 1가 %c에서 %c로 이동\n",from,to);
+   else
+   {
+      hanoi_tower(n-1,from,to,temp);
+      printf("원판 %d가 %c에서 %c로 이동\n",n,from,to);
+      hanoi_tower(n-1,temp,from,to);
+   }
+}
+
+void main()
+{
+   int n;
+   printf("하노이탑 입력 : ");
+   scanf("%d",&n);
+   printf("")
+   hanoi_tower(n,'A','B','C');
+}
